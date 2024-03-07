c77d296acb8a65cdfc234189661a064de6afa84d
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-03-07 19:46:36 +0530

    MyBox Component created and Applied useFrame Hook to give rotation properties.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import './App.css'
+import { useRef } from 'react'
+
+const MyBox = ({position,size,color}) => {
+  const ref = useRef()
+  useFrame((state,delta)=>{
+    ref.current.rotation.x+=delta
+    ref.current.rotation.y+=delta*1.5
+  })
+  return <mesh position={position} ref={ref}>
+    <boxGeometry args={size} />
+    <meshStandardMaterial color={color}/>
+  </mesh>
+}
+
 const App = () => {
 
   return (
     <>
       <Canvas>
         <directionalLight position={[0,0,2]}/>
-        <mesh position={[1.2,0,0]}>
-          <boxGeometry args={[2,2,2]} />
-          <meshStandardMaterial color={"#f00"}/>
-        </mesh>
+        
+        <MyBox position={[1.5,-1,1]} size={[1.5,1.5,1.5]} color={"#555"}/>
+        <MyBox position={[-1.5,-1,1]} size={[1.5,1.5,1.5]} color={"#999"}/>
+        <MyBox position={[1.5,1,1]} size={[1.5,1.5,1.5]} color={"#ccc"}/>
+        <MyBox position={[-1.5,1,1]} size={[1.5,1.5,1.5]} color={"#fff"}/>
       </Canvas>
     </>
   )
